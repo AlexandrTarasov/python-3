bc07947c21ad931cf908fc8e762b02dd01be1e38
AlexandrTarasov <usproinfo@gmail.com>
2017-11-17 12:53:17 +0200

    Update collect-list-filnames.py

--- collect-list-filnames.py
+++ collect-list-filnames.py
@@ -9,3 +9,18 @@ dirs = os.listdir( path )
 # This would print all the files and directories
 for file in dirs:
    print (file)
+
+
+
+
+from os import listdir
+from os.path import isfile
+from os.path import join as joinpath
+
+mypath = "content"
+for i in listdir(mypath):
+    if isfile(joinpath(mypath,i)): #only files
+        print i
+      
+      
+mytxt = filter(lambda x: x.endswith('.txt'), files) #only text files
